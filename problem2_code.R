73e57e62940a1fec1807a23de69849ccd142b14b
Johannes VK <johannesvk@protonmail.com>
2020-03-26 18:41:58 +0100

    Tip from Ingrid, gathering lm-objects in a list for use later.

--- problem2_code.R
+++ problem2_code.R
@@ -29,3 +29,20 @@ p_Grad.Rate = ggplot(College_2, aes(x = Grad.Rate, y = Outstate, color=color_dat
 
 gridExtra::grid.arrange(p_Private, p_Room.Board, p_Terminal, p_perc.alumni, p_Expend, p_Grad.Rate, nrow=3, ncol=2)
 
+lm_d_list = list()
+for(i in 1:10){
+  cat("i:", i, "\n")
+  lm_d_list[[i]] = lm(Outstate ~ poly(Terminal, i), data=College_2[train.ind, ])
+  cat("RSE: ", sum(lm_d_list[[i]]$residuals**2)/length(train.ind), "\n")
+}
+
+length(train.ind)
+print(class(list_models[[1]]))
+View(lm_d_list)
+summary(lm_d_list[[3]])
+summary(lm_d_list[[4]])
+
+# TODO:
+# Plot the results. Big plot, with 10 lines on might be messy. Maybe do d = 1, 2, 3, 6, 8, 10. A bit more pleasant to look at?
+# Choose an optimal degree of freedom, AIC, BIC or something. Possibly CV.
+# Use that number of degrees of freedom to compute a cubic spline interpolation, at the required quantiles.
